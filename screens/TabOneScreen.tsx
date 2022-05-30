906223cc6da0dc64e32221d18ab6a8c5a0a4ed17
lousybear <ashishkrse@gmail.com>
2022-05-30 14:02:35 +0530

    ViewPropTypes

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,32 +1,174 @@
-import { StyleSheet } from 'react-native';
+import {
+  Button,
+  StyleSheet,
+  TouchableOpacity,
+  View,
+  Text,
+  Image,
+} from "react-native";
+import { RootTabScreenProps } from "../types";
+import LottieView from "lottie-react-native";
+import React, { useRef, useEffect, useState } from "react";
+import coinFlip from "./93966-coin-flip-animation.json";
+import dollarCoin from "./../assets/images/1590-coin-flip.json";
+import dollarNote from "./../assets/images/dollar.jpg";
+//import coinToss from "./../assets/images/23227-coin-flip-rupee.json";
+export default function TabOneScreen({
+  navigation,
+}: RootTabScreenProps<"TabOne">) {
+  const animation = useRef(null);
+  const [showLottie, setShowLottie] = useState(2);
+  const [currentSelectedAmount, setCurrentSelectedAmount] = useState<Number>(0);
+  const betAmount = ["10", "20", "50", "100", "500"];
 
-import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
-import { RootTabScreenProps } from '../types';
+  const addMoney = (amount: Number) => {
+    const newAmount = currentSelectedAmount + amount;
+    setCurrentSelectedAmount(newAmount);
+  };
 
-export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
+  useEffect(() => {
+    // You can control the ref programmatically, rather than using autoPlay
+    // animation.current?.play();
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
+    <View style={styles.animationContainer}>
+      {/* {showLottie == 1 ? (
+        <LottieView
+          onAnimationFinish={() => {
+            setShowLottie(1);
+          }}
+          ref={animation}
+          style={{
+            width: 400,
+            height: 400,
+            backgroundColor: "#000",
+          }}
+          source={coinFlip}
+        />
+      ) : null}
+      {showLottie == 2 ? (
+        <LottieView
+          autoPlay
+          loop
+          ref={animation}
+          style={{
+            width: 200,
+            height: 300,
+            backgroundColor: "#000",
+          }}
+          source={dollarCoin}
+        />
+      ) : null} */}
+
+      <View
+        style={{
+          flexWrap: "wrap",
+          flex: 1,
+          flexDirection: "row",
+          justifyContent: "center",
+        }}
+      >
+        {betAmount.map((amount, index) => {
+          return (
+            <TouchableOpacity
+              key={index}
+              style={{ margin: 10 }}
+              onPress={() => {
+                const currentAmount = amount;
+                addMoney(Number(currentAmount));
+              }}
+            >
+              <Image
+                style={{ width: 80, height: 50 }}
+                source={dollarNote}
+                key={index}
+              />
+              <Text
+                style={{
+                  color: "black",
+                  position: "absolute",
+                  fontWeight: "700",
+                  fontSize: 20,
+                  left: "30%",
+                  top: "26%",
+                }}
+              >
+                ₹{amount}
+              </Text>
+            </TouchableOpacity>
+          );
+        })}
+      </View>
+
+      <View style={styles.xflex}>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => {
+            runAnimFrame(19, 45);
+          }}
+        >
+          <Text style={{ color: "white", fontWeight: "600" }}>HEAD</Text>
+        </TouchableOpacity>
+        <Text style={{ fontSize: 50, color: "white", fontWeight: "600" }}>
+          ₹{currentSelectedAmount}
+        </Text>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => {
+            animation.current.pause();
+            setShowLottie(1);
+            runAnimFrame(0, 19);
+          }}
+        >
+          <Text style={{ color: "white" }}>TAIL</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
+
+  function runAnimFrame(startFrame: number, endFrame: number) {
+    animation.current.play(startFrame, endFrame);
+  }
 }
 
 const styles = StyleSheet.create({
+  animationContainer: {
+    backgroundColor: "#000",
+    alignItems: "center",
+    flex: 1,
+  },
+  buttonContainer: {
+    paddingTop: 20,
+  },
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   separator: {
     marginVertical: 30,
     height: 1,
-    width: '80%',
+    width: "80%",
+  },
+  xflex: {
+    flexDirection: "row",
+    width: "100%",
+    justifyContent: "space-between",
+    marginHorizontal: 20,
+    position: "absolute",
+    bottom: 10,
+  },
+  button: {
+    paddingHorizontal: 30,
+    paddingVertical: 20,
+    backgroundColor: "green",
+    color: "white",
+    margin: 10,
+    borderRadius: 6,
   },
 });
--- screens/TabTwoScreen.tsx
+++ screens/TabTwoScreen.tsx
@@ -1,16 +1,68 @@
-import { StyleSheet } from 'react-native';
-
+import { useState } from 'react';
+import { StyleSheet, TouchableOpacity,TextInput  } from 'react-native';
+const webSocketServer = require("websocket").server;
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
+import axios from 'axios';
+const baseUrl ='https://fategamev1.herokuapp.com';
 
 export default function TabTwoScreen() {
+
+
+const client = new WebSocket('ws://13.126.249.51:8000');
+// Invoking get method to perform a GET request
+//const client = new webSocketServer('ws://fategamev1.herokuapp.com:8000');
+ const [count, setCount] = useState<string>("16");
+ const [side,setSide]=useState<string>("");
+
+client.onopen = () => {
+  console.log("WebSocket Client Connected");
+};
+
+client.onmessage = (message:any) => {
+  console.log(message);
+};
+client.onmessage = (message:any) => {
+    const dataFromServer = JSON.parse(message.data);
+    setCount(dataFromServer.winner);
+    console.log("data============"+dataFromServer);
+  };
+   const onPress = () =>{ 
+     try {
+      const response =  axios.post(`${baseUrl}/api/v1/coin`, {
+       "amt": count ,
+        "side": "T" ,
+        "userId" : "usr1233"
+      });
+     response.then((result)=>{
+      console.log(result.status)
+     })
+    } catch (error) {
+      alert("An error has occurred");
+    
+    } 
+    console.log("Api2 called")};
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Tab Two</Text>
+      
+      <TextInput  style={styles.input}
+        value={count}
+         placeholder="useless placeholder"
+      />
+       <TextInput  style={styles.input} value={side}  placeholder="useless side" />
+       
+      <TouchableOpacity onPress={onPress} style={styles.button} > 
+       
+       <Text>Bet</Text>
+      </TouchableOpacity>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="/screens/TabTwoScreen.tsx" />
     </View>
   );
+
+ 
 }
 
 const styles = StyleSheet.create({
@@ -27,5 +79,15 @@ const styles = StyleSheet.create({
     marginVertical: 30,
     height: 1,
     width: '80%',
+  },
+  button: {
+    alignItems: "center",
+    backgroundColor: "#DDDDDD",
+    padding: 10
+  }, input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
   },
 });
